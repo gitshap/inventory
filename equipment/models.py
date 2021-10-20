53b106021355a37b5e19bba396b05020f17c1de6
unknown <carl.cruz@shap.edu.ph>
2021-10-20 09:36:03 +0800

    added image field

--- equipment/models.py
+++ equipment/models.py
@@ -5,6 +5,7 @@ class Equipment(models.Model):
     name = models.CharField(max_length=255)
     label = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to ='uploads/')
 
     def __str__(self):
         return f"{self.name}({self.label}) is in {self.location}"
--- inventory_qr/settings.py
+++ inventory_qr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r5&w8azo^ho=&+$+4kt1expth5^j-odu+0e6n8u^bfbk+j5mup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

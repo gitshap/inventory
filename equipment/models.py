4c3f0227e0caea991f2ab30e318b25a09d545fdb
Carl <cruzthecarl10@gmail.com>
2021-10-28 15:05:00 +0800

    modified str

--- equipment/models.py
+++ equipment/models.py
@@ -9,4 +9,6 @@ class Equipment(models.Model):
     note = models.TextField(default="Text here")
 
     def __str__(self):
-        return f"{self.name}({self.label}) is in {self.location}"
+        return (
+            f"{self.name}({self.label}) is in {self.location} with the note {self.note}"
+        )

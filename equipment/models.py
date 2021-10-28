9a65621b83bdfaca5c98d1d67b1a183432700895
Carl <cruzthecarl10@gmail.com>
2021-10-28 15:01:28 +0800

    added default image and note model, modified page

--- equipment/models.py
+++ equipment/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Equipment(models.Model):
     name = models.CharField(max_length=255)
     label = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    image = models.ImageField(upload_to ='uploads/')
+    image = models.ImageField(upload_to="uploads/", default="default.png")
+    note = models.TextField(default="Text here")
 
     def __str__(self):
         return f"{self.name}({self.label}) is in {self.location}"

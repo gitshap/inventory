2f4817cbfc1d6099be816f703c22195b963b5591
unknown <carl.cruz@shap.edu.ph>
2021-11-10 09:37:24 +0800

    removed created for the time period

--- equipment/models.py
+++ equipment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from datetime import datetime
+from django.utils import timezone
 
 class Equipment(models.Model):
     """ Equipment Model """
@@ -8,8 +9,7 @@ class Equipment(models.Model):
     location = models.CharField(max_length=255)
     image = models.ImageField(upload_to="uploads/", default="default.png")
     note = models.TextField(default="Text here")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return (

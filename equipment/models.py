808e318313a57a692c1bed564a75b5730bdbeb80
Carl <shapit@SHAP.EDU.PH>
2021-11-12 13:42:37 +0800

    updated created and updated at

--- equipment/models.py
+++ equipment/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from datetime import datetime
-from django.utils import timezone
+import uuid
+
+
 
 class Equipment(models.Model):
     """ Equipment Model """
     name = models.CharField(max_length=255)
-    label = models.CharField(max_length=255, unique=True)
+    label = models.CharField(max_length=255, primary_key=True)
     location = models.CharField(max_length=255)
     image = models.ImageField(upload_to="uploads/", default="default.png")
     note = models.TextField(default="Text here")
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):

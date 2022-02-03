e7f3186c51394967642b88c3874eff853775c093
Carl <48541404+carljcruz@users.noreply.github.com>
2022-02-03 14:38:28 +0800

    added equipment, license, users page

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from equipment.views import equipment_view, csv_to_import, home_view, login_view, licenses_view
+from equipment.views import equipment_view, csv_to_import, list_users ,home_view, login_view, licenses_view, list_equipments
 
 urlpatterns = [
     path('', home_view, name='home_view'),
     path('csv/', csv_to_import, name='csv_importer'),
     path('login/', login_view, name='login_view'),
     path('equipment/<str:label>/', equipment_view, name='equipment-view'),
-     path('licenses/', licenses_view, name='licenses_view'),
+    path('licenses/', licenses_view, name='list_licenses'),
+    path('equipments/', list_equipments, name='list_equipments'),
+    path('users/', list_users, name='list_users'),
     
 ]
 
--- equipment/views.py
+++ equipment/views.py
@@ -61,8 +61,28 @@ def login_view(request):
 
 def licenses_view(request):
     licenses = Licenses.objects.all()
-    template_name = 'licenses.html'
+    template_name = 'all_details/all_licenses.html'
     context = {
         'licenses': licenses
+    }
+    return render(request, template_name, context=context)
+
+
+def list_equipments(request):
+    equipments = Equipment.objects.all()
+    template_name = 'all_details/all_equipment.html'
+    context = {
+        'equipments': equipments
+    }
+    return render(request, template_name, context=context)
+
+
+
+
+def list_users(request):
+    staffs = Staff.objects.all()
+    template_name = 'all_details/all_users.html'
+    context = {
+        'staffs': staffs
     }
     return render(request, template_name, context=context)
